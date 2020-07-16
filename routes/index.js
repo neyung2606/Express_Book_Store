4be285a27a185c2f8e36ae31b373f4f1d73ba6ad
Nguyên Trần Đặng Văn <tdvannguyen@gmail.com>
2020-07-16 23:35:47 +0700

    show a book

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,18 @@ const router = express.Router();
 const Book = require("../models/book")
 
 
-router.get("/", (req, res) => {
-    Book.find({}).then(books => {
-        res.render('home', {books: books})
-    })
+router.get("/", async (req, res) => {
+    const books = await Book.find({});
+    res.render("home", {books: books})
 });
+router.get("/:name", async (req, res) => {
+    const book = await Book.find({name: req.params.name});
+    const books = await Book.find({}).limit(4);
+    res.render("product-single", {
+        book: book,
+        books: books
+    });
+})
 router.get("/shop", (req, res) => {
     res.render("shop")
 })
